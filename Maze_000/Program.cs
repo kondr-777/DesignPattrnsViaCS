1cf4fa2adf2a399134432bc68be4ebe30826edaf
kondratev <kondr.777@yandex.ru>
2022-11-19 15:02:25 +0300

    First load code.

---
+++ Maze_000/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zame0;
+
+namespace Maze_000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MazeGame mg = new MazeGame();
+            Maze m = mg.CreateMaze();
+            while(true)
+            {
+                ConsoleKeyInfo k = Console.ReadKey(false);
+                Console.WriteLine(k.Key);
+                if(k.Key == ConsoleKey.UpArrow || k.Key == ConsoleKey.DownArrow || k.Key == ConsoleKey.LeftArrow || k.Key == ConsoleKey.RightArrow)
+                {
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Press key arrows.");
+                }
+                
+            }
+        }
+    }
+}
